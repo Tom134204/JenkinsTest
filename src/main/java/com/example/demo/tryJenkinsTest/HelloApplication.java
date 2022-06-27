f87cb26d0e2bab6af04491443e4b20ac8be8be03
Tom134204 <merck.goodgame@gmail.com>
2022-06-27 19:03:11 +0900

    update index.html for jenkins test6...

--- src/main/java/com/example/demo/tryJenkinsTest/HelloApplication.java
+++ src/main/java/com/example/demo/tryJenkinsTest/HelloApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloApplication
 {
 
+	// for Jenbkins Test...
 	public static void main(String[] args)
 	{
 		SpringApplication.run(HelloApplication.class, args);
